2eb794d4d8be915b6a281dffb9e46e43c47d88f6
Ashley Maceli and Elissa Sosland <elissa@>
2016-05-03 11:31:06 -0700

    Create database with doctors, patients and specialties tables and passing BDD test for instantiating Doctor object

---
+++ src/main/java/Doctor.java
@@ -0,0 +1,9 @@
+public class Doctor {
+  private int id;
+  private int specialty;
+  private String name;
+
+  public Doctor(String name) {
+    this.name = name;
+  }
+}
---
+++ src/test/java/DoctorTest.java
@@ -0,0 +1,25 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class DoctorTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hospital_admin_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM doctors *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Doctor_instantiatesCorrectly_true() {
+    Doctor myDoctor = new Doctor("Doctor Smith");
+    assertEquals(true, myDoctor instanceof Doctor);
+  }
+}
